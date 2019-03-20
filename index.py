abecd0c605e59ceb18a5f22744f2f0809bcb3e95
Anwesh43 <anweshthecool0@gmail.com>
2019-03-20 11:42:48 +0530

    one more example

--- index.py
+++ index.py
@@ -1,2 +1,4 @@
 from random_module import sum
 sum(10, 20)
+from random_module.DictIter import iterateDict
+iterateDict({"a": 1, "b2" : 2, "c" : 3}) 
---
+++ random_module/DictIter.py
@@ -0,0 +1,3 @@
+def iterateDict(dict1):
+    for key, value in dict1.items():
+        print("{0} and {1}".format(key, value))
